524d7b68abb32cfad21f94f19b0247bac3273d5d
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-10-24 19:42:49 -0300

    Testando com o sensor com o valor de 500

--- sensor-luminosidade/sensor-luminosidade.ino
+++ sensor-luminosidade/sensor-luminosidade.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+int portaLed = 10; //Porta a ser utilizada para ligar o led
+int portaLDR = A5; //Porta analógica utilizada pelo LDR
 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(portaLed, OUTPUT); //Define a porta do Led como saída
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  int estado = analogRead(portaLDR);  //Lê o valor fornecido pelo LDR
 
+  Serial.println(estado);
+  // Caso o valor lido na porta analógica seja maior do que
+  // 800, acende o LED
+  // Ajuste o valor abaixo de acordo com o seu circuito
+  if (estado > 500)
+  {
+    digitalWrite(portaLed, HIGH);
+  }
+  else  //Caso contrário, apaga o led
+  {
+    digitalWrite(portaLed, LOW);
+  }
 }
